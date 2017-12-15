d975eec878a5f5c69124d21780788fc4b1874c3a
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2017-12-15 18:28:39 +0530

    ICFJ-202 - Adds new layout for col-4 column.

--- main.js
+++ main.js
@@ -126,3 +126,19 @@ ProtoGraph.Card.toArticle.prototype.renderScreenshot = function (data) {
       }}/>,
     this.options.selector);
 }
+
+ProtoGraph.Card.toArticle.prototype.renderCol4BigImgTxtLayout = function (data) {
+  this.mode = 'col4_big_image_text';
+  ReactDOM.render(
+    <ArticleCard
+      dataURL={this.options.data_url}
+      schemaURL={this.options.schema_url}
+      optionalConfigURL={this.options.configuration_url}
+      optionalConfigSchemaURL={this.options.configuration_schema_url}
+      clickCallback={this.options.onClickCallback}
+      mode={this.mode}
+      ref={(e) => {
+        this.containerInstance = this.containerInstance || e;
+      }} />,
+    this.options.selector);
+}
